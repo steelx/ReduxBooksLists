02d2d0a26459b72e89de452d75fa2f44a55849d4
Steel <ajinkya@riva.co>
2016-04-12 12:06:17 +0530

    combineReducers to be called inside react with connect

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+    return [
+        {title: 'JavaScript the good parts'},
+        {title: 'JS the right way'},
+        {title: 'Understanding Node.js'},
+        {title: 'React Redux Starter'},
+        {title: 'JavaScript the Bad parts'}
+    ];
+}
